d699519cabb804213f8ef1bb9ef4ae7de9756b49
Hashan Shalitha <h.shalitha@webquarters.com>
2020-07-09 20:30:50 +0530

    Added Mongoose & created Hero Model

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const home = require('./route/home');
 const heroes = require('./route/heroes');
 const authenticator = require('./middleware/authenticator');
@@ -11,6 +12,11 @@ app.use(authenticator);
 app.use(logger);
 app.use('/', home);
 app.use('/api/heroes', heroes);
+
+mongoose
+    .connect("mongodb://localhost/herodb", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to Db successfully ... "))
+    .catch(err => console.log("Error has occured while connecting to db : ", err));
 
 app.listen(PORT, () => {
     console.log("Listning on Port : " + PORT);
