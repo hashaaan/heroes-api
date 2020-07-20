13f419f52128edf36fd82e14b63b0e73702479ec
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2020-07-20 18:07:45 +0530

    PUT and DELETE method using mongoose

--- route/heroes.js
+++ route/heroes.js
@@ -2,12 +2,6 @@ const express = require('express');
 const Hero = require('../model/hero');
 const router = express.Router();
 
-let heroesArray = [
-    { id: 1, name: 'Captain America' },
-    { id: 2, name: 'Iron Man' },
-    { id: 3, name: 'Black Widow' }
-];
-
 router.get('/', async (req,res) => {
     try {
         // Hero.find({ deceased: true });
@@ -16,9 +10,9 @@ router.get('/', async (req,res) => {
         // Hero.find({ likeCount: { $nin: [3000, 10000] } })
         // Hero.find().or([{ likeCount: 3000 }, { likeCount: 5000 }]) // should likeCount 3000 or 5000
         let heroes = await Hero.find()
-            .or([{ likeCount: 3000 }, { likeCount: 5000 }])
-            .sort({name:'asc'})
-            .select({ name: 1, deceased: 1, likeCount: 1 })
+            //.or([{ likeCount: 3000 }, { likeCount: 5000 }])
+            .sort({name:'asc'});
+            //.select({ name: 1, deceased: 1, likeCount: 1 })
             //.countDocuments();
         res.send(heroes);
     } catch (e) {
@@ -62,6 +56,7 @@ router.post('/', async (req,res) => {
 
 });
 
+/*
 router.put('/:heroId', async (req,res) => {
 
     let heroId = req.params.heroId;
@@ -76,22 +71,29 @@ router.put('/:heroId', async (req,res) => {
     }
 
 });
+*/
 
-router.delete('/:heroId', (req,res) => {
-    let heroId = parseInt(req.params.heroId);
-    let hero = heroesArray.find(hero => hero.id === heroId);
-
-    if(!hero) {
-        res.status(404).send("The given id does not exist on our server");
+router.put('/:heroId', async (req,res) => {
+    try {
+        let hero = await Hero.findByIdAndUpdate(
+            { _id: req.params.heroId },
+            { $set: { name: req.body.heroName } },
+            { new: true, useFindAndModify: false }
+        );
+        res.send(hero);
+    } catch(e) {
+        return res.status(500).send(e.message); 
     }
 
-    let heroIndex = heroesArray.indexOf(hero);
+});
 
-    heroesArray.splice(heroIndex,1); // param1: index param2: no of nodes
-
-    console.log(heroesArray);
-
-    res.send(hero);
+router.delete('/:heroId', async (req,res) => {
+    try {
+        let hero = await Hero.findByIdAndDelete({ _id: req.params.heroId });
+        res.send(hero);
+    } catch(e) {
+        return res.status(500).send(e.message);  
+    }
 });
 
 module.exports = router;
