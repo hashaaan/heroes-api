316df5a267e5cb8530caaff84a6cd8f1c6556177
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2020-07-27 18:49:17 +0530

    Add cors fix

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const home = require('./route/home');
-const heroes = require('./route/heroes');
-const authenticator = require('./middleware/authenticator');
-const logger = require('./middleware/logger');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const home = require("./route/home");
+const heroes = require("./route/heroes");
+const authenticator = require("./middleware/authenticator");
+const logger = require("./middleware/logger");
 const app = express();
 const PORT = 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(authenticator);
 app.use(logger);
-app.use('/', home);
-app.use('/api/heroes', heroes);
+app.use("/", home);
+app.use("/api/heroes", heroes);
 
 mongoose
-    .connect("mongodb://localhost/herodb", { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to Db successfully ... "))
-    .catch(err => console.log("Error has occured while connecting to db : ", err));
+  .connect("mongodb://localhost/herodb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Db successfully ... "))
+  .catch((err) =>
+    console.log("Error has occured while connecting to db : ", err)
+  );
 
 app.listen(PORT, () => {
-    console.log("Listning on Port : " + PORT);
+  console.log("Listning on Port : " + PORT);
 });
